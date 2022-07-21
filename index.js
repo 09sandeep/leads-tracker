83b41ca120f018838e8bfaec187ae4411b9d1a93
Sandeep Kumar <sandeep09kumar01@gmail.com>
2022-07-21 17:08:25 +0530

    before local storage

---
+++ index.js
@@ -0,0 +1,22 @@
+let myLeads = [];
+const inputEL = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+
+inputBtn.addEventListener("click", function(){
+    myLeads.push(inputEL.value);
+    inputEL.value = '';
+    renderLeads();
+})
+
+function renderLeads(){
+    let listItems ="";
+    for(let i = 0; i < myLeads.length; i++){
+        listItems += `<li>
+        <a target = '_blank' href='${myLeads}'> ${myLeads[i]}
+        </a>
+        </li>`;
+    }
+    ulEl.innerHTML = listItems;
+}
+
