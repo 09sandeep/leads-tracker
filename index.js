8e0751c30ad43cd16a9154ae1a6227bf7ffa40a2
Sandeep Kumar <sandeep09kumar01@gmail.com>
2022-07-21 18:56:04 +0530

    save tab

--- index.js
+++ index.js
@@ -3,12 +3,21 @@ const inputEL = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 const delBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("save-tab");
 
 const leadsFromLocal = JSON.parse(localStorage.getItem("myLeads"));
 if(leadsFromLocal){
     myLeads = leadsFromLocal;
     renderLeads(myLeads);
 }
+
+tabBtn.addEventListener("click", () => {
+    chrome.tabs.query({active : true, currentWindow : true}, function(tabs){
+        myLeads.push(tabs[0].url);
+        localStorage.setItem("myLeads", JSON.stringify(myLeads));
+        renderLeads(myLeads);
+    })
+})
 function renderLeads(leads){
     let listItems ="";
     for(let i = 0; i < leads.length; i++){
